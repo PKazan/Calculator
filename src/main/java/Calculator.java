5516895a8e9206354527ca44f6f11097fafa7b80
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 20:42:49 +0100

    added methods for addition, subtraction, multiplication and division

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,23 +1,25 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(123,27));
-        System.out.println(subtractTwoNumbers(123,27));
-        System.out.println(multiplyTwoNumbers(123,27));
-        System.out.println(divideTwoNumbers(123,27));
+        System.out.println(addTwoNumbers(123, 27));
+        System.out.println(subtractTwoNumbers(123, 27));
+        System.out.println(multiplyTwoNumbers(123, 27));
+        System.out.println(divideTwoNumbers(123, 27));
 
     }
 
     public static long addTwoNumbers(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
+
     public static long subtractTwoNumbers(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+
     public static long multiplyTwoNumbers(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
+
     public static long divideTwoNumbers(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
-
 }
